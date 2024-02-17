a8684122bf29ff98ceaeece331aaeef23ef96125
Farid Mammadli <fmammadli12101@ada.edu.az>
2024-02-17 23:49:22 +0400

    Printing Upgraded to be dynamic!

--- app/src/main/java/a1/db/migration/faridm5/TablePrinter.java
+++ app/src/main/java/a1/db/migration/faridm5/TablePrinter.java
@@ -1,22 +1,35 @@
 package a1.db.migration.faridm5;
 
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 public class TablePrinter {
     public static void printTables(Statement statement) {
+        printTable(statement, "STUDENTS");
+        printTable(statement, "INTERESTS");
+    }
+
+    private static void printTable(Statement statement, String tableName) {
         try {
-            ResultSet rs = statement.executeQuery("SELECT * FROM STUDENTS");
+            ResultSet rs = statement.executeQuery("SELECT * FROM " + tableName);
+            ResultSetMetaData metaData = rs.getMetaData();
+            int columnCount = metaData.getColumnCount();
+
+            System.out.println("Table: " + tableName);
             while (rs.next()) {
-                System.out.println("STUDENTS - ID: " + rs.getInt("ST_ID") + ", Name: " + rs.getString("ST_NAME") + ", Last Name: " + rs.getString("ST_LAST"));
+                for (int i = 1; i <= columnCount; i++) {
+                    String columnName = metaData.getColumnName(i);
+                    String value = rs.getString(i);
+                    System.out.print(columnName + ": " + value + ", ");
+                }
+                System.out.println(); // Move to the next line after printing a row
             }
-            rs = statement.executeQuery("SELECT * FROM INTERESTS");
-            while (rs.next()) {
-                System.out.println("INTERESTS - Student ID: " + rs.getInt("STUDENT_ID") + ", Interest: " + rs.getString("INTEREST"));
-            }
-            System.out.println("Tables are printed!");
-        } catch (Exception e) {
-            System.out.println("Seen an error while printing the tables!");
+            System.out.println("End of " + tableName + " table.");
+        } catch (SQLException e) {
+            System.out.println("Seen an error while printing the " + tableName + " table: " + e.getMessage());
         }
     }
 }
+
